353e29648d3d1146c7d0cbe7cd69aafa920d2a5c
Thomas Tanner <trtanner@btinternet.com>
2017-07-24 22:08:37 +0100

    Unrelated, but why are these utf8 conversions necessary?

--- source/content/inforss/inforssIO.js
+++ source/content/inforss/inforssIO.js
@@ -232,6 +232,7 @@ function inforssCopyRemoteToLocal1Callback(step, status, path, callbackOriginal)
 
         if (str.length > 0)
         {
+          //FIXME Why would you convert utf-8 to utf-8?
           var uConv = Components.classes['@mozilla.org/intl/utf8converterservice;1'].createInstance(Components.interfaces.nsIUTF8ConverterService);
           str = uConv.convertStringToUTF8(str, "UTF-8", false);
         }
