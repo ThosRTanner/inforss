c1b8bdcc62d176e497972353568c90c0e1faf72c
ThosRTanner <ThosRTanner@users.noreply.github.com>
2018-11-15 09:16:38 +0000

    Create hintcheck.js

---
+++ hintcheck.js
@@ -0,0 +1,19 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
+
+function main() {
+  var array = new Array();
+  return 'Hello, World!';
+  var a=eval("1");
+  var b=eval(a);
+}
+
+main();
